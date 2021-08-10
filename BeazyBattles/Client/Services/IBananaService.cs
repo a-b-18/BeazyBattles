1931cbe5971c7f207ff41d9f6f5b648c519765b8
AZBZ34 <alexbeazley18@outlook.com>
2021-08-11 05:31:25 +1000

    79 complete. Bananas are now put from client and updated into sql db.

--- BeazyBattles/Client/Services/IBananaService.cs
+++ BeazyBattles/Client/Services/IBananaService.cs
@@ -10,7 +10,7 @@ namespace BeazyBattles.Client.Services
         event Action OnChange;
         int Bananas { get; set; }
         void EatBananas(int amount);
-        void AddBananas(int amount);
+        Task AddBananas(int amount);
         Task GetBananas();
     }
 }
