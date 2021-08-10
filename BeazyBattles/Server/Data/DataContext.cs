b6d76b5af277b7f95f18791a9ece831d99ef81ca
AZBZ34 <alexbeazley18@outlook.com>
2021-08-11 05:42:23 +1000

    80 complete. UserUnits in db.

--- BeazyBattles/Server/Data/DataContext.cs
+++ BeazyBattles/Server/Data/DataContext.cs
@@ -17,5 +17,7 @@ namespace BeazyBattles.Server.Data
         public DbSet<Unit> Units { get; set; }
 
         public DbSet<User> Users { get; set; }
+
+        public DbSet<UserUnit> UserUnits { get; set; }
     }
 }
