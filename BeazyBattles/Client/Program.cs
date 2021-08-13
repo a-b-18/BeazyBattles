2258f9d5679237842b52310f418490a9626ba9a3
AZBZ34 <alexbeazley18@outlook.com>
2021-08-13 21:40:32 +1000

    91 complete. Leaderboard page added.

--- BeazyBattles/Client/Program.cs
+++ BeazyBattles/Client/Program.cs
@@ -30,6 +30,7 @@ namespace BeazyBattles.Client
             builder.Services.AddOptions();
             builder.Services.AddAuthorizationCore();
             builder.Services.AddScoped<IAuthService, AuthService>();
+            builder.Services.AddScoped<ILeaderboardService, LeaderboardService>();
 
             await builder.Build().RunAsync();
         }
---
+++ BeazyBattles/Client/Services/ILeaderboardService.cs
@@ -0,0 +1,14 @@
+﻿using BeazyBattles.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeazyBattles.Client.Services
+{
+    public interface ILeaderboardService
+    {
+        IList<UserStatistic> Leaderboard { get; set; }
+        Task GetLeaderboard();
+    }
+}
