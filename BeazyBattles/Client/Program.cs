4baa2bab762dcc896c4fb17d5c91847fc3a5a06a
AZBZ34 <alexbeazley18@outlook.com>
2021-08-14 14:28:47 +1000

    99 complete. Fight button works with battle service.

--- BeazyBattles/Client/Program.cs
+++ BeazyBattles/Client/Program.cs
@@ -31,6 +31,8 @@ namespace BeazyBattles.Client
             builder.Services.AddAuthorizationCore();
             builder.Services.AddScoped<IAuthService, AuthService>();
             builder.Services.AddScoped<ILeaderboardService, LeaderboardService>();
+            builder.Services.AddScoped<IBattleService, BattleService>();
+
 
             await builder.Build().RunAsync();
         }
---
+++ BeazyBattles/Client/Services/IBattleService.cs
@@ -0,0 +1,13 @@
+﻿using BeazyBattles.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeazyBattles.Client.Services
+{
+    public interface IBattleService
+    {
+        Task<BattleResult> StartBattle(int opponentId);
+    }
+}
