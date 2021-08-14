b4875e350412bf71f51819c4bc5e69975a5e196c
AZBZ34 <alexbeazley18@outlook.com>
2021-08-14 13:03:25 +1000

    95 complete. database updated for battles.

--- BeazyBattles/Server/Migrations/20210814025715_Battles.Designer.cs
+++ BeazyBattles/Server/Migrations/20210814025715_Battles.Designer.cs
@@ -4,14 +4,16 @@ using BeazyBattles.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BeazyBattles.Server.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20210814025715_Battles")]
+    partial class Battles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
